4089dd18cdcba48eb3027de80f8d04908a483de8
Jesús Soto <jealsotomont@gmail.com>
2025-05-26 11:09:17 -0600

    feat: added a new paragraph for location and fix class selectors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $name = document.querySelector('name');
-const $blog = document.querySelector('#blog');
+const $name = document.querySelector('.name');
+const $blog = document.querySelector('.blog');
 const $location = document.querySelector('.location');
 
 async function displayUser(username) {
   $name.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $name.textContent = '${data.name}';
-  $blog.textContent = '${data.blog}';
-  $location.textContent = '${data.location}';
+  $name.textContent = `${data.name}`;
+  $blog.textContent = `${data.blog}`;
+  $location.textContent = `${data.location}`;
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  $name.textContent = `Algo salió mal: ${err}`
+  $name.textContent = `Algo salió mal: ${err}`;
 }
 
 displayUser('stolinski').catch(handleError);
