f206ecf8d84861932b3f241d3193427a22257d87
Jesús Soto <jealsotomont@gmail.com>
2025-05-26 11:17:14 -0600

    fix: assign data to response in displayuser function add comments and then in display

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,19 +4,21 @@ const $name = document.querySelector('.name');
 const $blog = document.querySelector('.blog');
 const $location = document.querySelector('.location');
 
+/* Tal vez le falta el response y error en los parametros */
 async function displayUser(username) {
   $name.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  data = response;
   console.log(data);
   $name.textContent = `${data.name}`;
   $blog.textContent = `${data.blog}`;
   $location.textContent = `${data.location}`;
 }
-
+/* El manejador de error  */
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
   $name.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').then(response).catch(handleError);
